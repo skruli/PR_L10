1fb1f6c862d76be5dccd204c6c301f54a1e6abee
skruli_ <grzesiek.789123@gmail.com>
2021-05-14 12:12:20 +0200

    L10

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-
+const axios = require('axios');
 
 class LoginForm extends Component {
 
@@ -10,6 +10,11 @@ class LoginForm extends Component {
         },
         errors: {}
     };
+
+    handleChangeRoute = () => {
+        this.props.history.push('/');
+    };
+ 
 
     validate = () => {
       const errors = {};
@@ -32,7 +37,22 @@ class LoginForm extends Component {
         this.setState({errors: errors || {}});
         if (errors) return;
 
-        console.log("submit - np. zapytanie do serwera");
+        axios({
+            method: 'post',
+            url: 'http://localhost:3001/api/user/auth',
+            data: {
+                login: this.state.account.username,
+                password: this.state.account.password
+            }
+        }).then((response) => {
+            localStorage.setItem('token', response.data.token);
+           this.handleChangeRoute();
+        }).catch((error) => {
+            const errors = {};
+           errors.password = 'Given username does\'t exists or password is wrong!';
+           this.setState({errors: errors || {}});
+            console.log(error);
+        });
     };
 
     handleChange = (event) => {
